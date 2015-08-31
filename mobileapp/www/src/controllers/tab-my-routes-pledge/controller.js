1eae8ad2a0a2a6c230d0fff179fd9a7f4df54746
Kara Yu <lele.yu@gmail.com>
2015-08-31 14:25:13 -0700

    wip pledges page

--- mobileapp/www/src/controllers/tab-my-routes-pledge/controller.js
+++ mobileapp/www/src/controllers/tab-my-routes-pledge/controller.js
@@ -7,7 +7,7 @@ var TEMPLATE_URL = CONTROLLER_URL.replace('controller.js','view.html');
 var CONTROLLER_PATH = URI(CONTROLLER_URL).path();
 CONTROLLER_PATH = CONTROLLER_PATH.substring(CONTROLLER_PATH.indexOf('/src/controllers/'));
 
-var ROUTE_URL = '/my-routes/:routeId';
+var ROUTE_URL = '/my-routes/:routeId/pledge';
 var MODULE_NAME = 'mainApp'+CONTROLLER_PATH.replace('/src','').replace('/controller.js','').replace(/\//g,'.');
 var CONTROLLER_NAME = MODULE_NAME.replace(/\./g,'_').replace(/-/g,'_');
 document.APP_MODULES.push(MODULE_NAME);
@@ -15,7 +15,7 @@ document.APP_MODULES.push(MODULE_NAME);
 console.log(MODULE_NAME, "Registering route", ROUTE_URL);
 angular.module(MODULE_NAME, ['ionic'])
   .config(function($stateProvider) {
-    $stateProvider.state('tab.my-routes-detail', {
+    $stateProvider.state('tab.my-routes-pledge', {
       url: ROUTE_URL,
       views: {
         'tab-my-routes': {
@@ -25,33 +25,42 @@ angular.module(MODULE_NAME, ['ionic'])
       }
     });
   })
-  .controller(CONTROLLER_NAME, function($scope, $stateParams, tripSearchService, userService, $state) {
+  .controller(CONTROLLER_NAME, function($scope, $stateParams, tripSearchService, userService) {
       $scope.route = {};
+      $scope.pledgeTotal = 0;
+      $scope.selectedIndex = 1;
 
-      $scope.trips = [];
-      $scope.refreshTrips = function(route) {
-        tripSearchService.getMyTrips().then(function(trips) {
-          $scope.trips = trips;
-        });
-      }
+      $scope.pledges = [
+        {
+          index: 0,
+          name: "One Trip",
+          price: 5.00,
+          description: "Be one of the pioneers of Sao Paolo's new subway! Guarantee your spot by buying a ticket on YOUR ROUTE now"
+        },
+        {
+          index: 1,
+          name: "Five Pack",
+          price: 20.00,
+          description: "Good things come in bulk. Give more support to Sao Paolo's subway system and save! Buy four train tickets and get one free!"
+        }
+      ];
 
       $scope.$on('$ionicView.beforeEnter', function(){
         tripSearchService.getRoute($stateParams.routeId).then(function(route) {
           $scope.route = route;
         });
-        $scope.refreshTrips($scope.route);
       });
-
-      $scope.goRoutePledge = function(route) {
-        $state.go('tab.my-routes-pledge', {routeId: route.routeId});
-      }
 
       $scope.$on('$ionicView.beforeLeave', function(){
         $scope.route = {};
+        $scope.pledges = [];
       });
 
+      $scope.selectPledge = function(pledge) {
+        $scope.pledgeTotal = $scope.pledgeTotal + pledge.price;
+        $scope.selectedIndex = pledge.index;
+      }
   })
 
-  
 })();
 
