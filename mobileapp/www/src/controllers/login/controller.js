60afe708af2f7bdfa0f42ec95874366d95caa15d
Vimal Bhalodia <vimal@alum.mit.edu>
2015-09-05 19:56:43 -0700

    updated fb login controller

--- mobileapp/www/src/controllers/login/controller.js
+++ mobileapp/www/src/controllers/login/controller.js
@@ -12,25 +12,6 @@ var MODULE_NAME = 'mainApp'+CONTROLLER_PATH.replace('/src','').replace('/control
 var CONTROLLER_NAME = 'LoginController';
 document.APP_MODULES.push(MODULE_NAME);
 
-function displayData($http, access_token)
-{
-    $http.get("https://graph.facebook.com/v2.2/me", {params: {access_token: access_token, fields: "name,gender,location,picture", format: "json" }}).then(function(result) {
-        var name = result.data.name;
-        var gender = result.data.gender;
-        var picture = result.data.picture;
-
-        var htmlHeader = "<div class='card avatar'><div class='item item-avatar header'><img class='profile-img' src='"+picture.data.url+"'> <h2>"+name+"</h2></div></div>";
-
-        document.getElementById("profileHeader").innerHTML = htmlHeader;
-        document.getElementById("login-button").className = "hide";
-
-        document.getElementsByClassName("sign-in-text")[0].className = "sign-in-text hide";
-        document.getElementsByClassName("success")[0].className -= "hide";
-        
-    }, function(error) {
-        alert("Error: " + error);
-    });
-}
 
 angular.module(MODULE_NAME, ['ionic', 'ngStorage', 'ngCordova'])
   .config(function($stateProvider) {
@@ -44,16 +25,44 @@ angular.module(MODULE_NAME, ['ionic', 'ngStorage', 'ngCordova'])
       }
     });
   })
-  .controller(CONTROLLER_NAME, function($scope, $state, $cordovaOauth, $http, $stateParams, $localStorage, $location) {
+  .controller(CONTROLLER_NAME, function($scope, $state, $cordovaOauth, $http, $stateParams, $localStorage, $location, userService) {
     console.log("Instantiating controller", CONTROLLER_NAME);
     window.cordovaOauth = $cordovaOauth;
     window.http = $http;
     $scope.loggedIn = false;
+
+
+    var displayData = function(access_token)
+    {
+        $http.get("https://graph.facebook.com/v2.2/me", {params: {access_token: access_token, fields: "name,gender,location,picture", format: "json" }}).then(function(result) {
+            var name = result.data.name;
+            var gender = result.data.gender;
+            var picture = result.data.picture;
+
+            currentUser = userService.getCurrentUser();
+            currentUser.name = name.split(' ')[0];
+            currentUser.photo_url = picture.data.url;
+
+
+            var htmlHeader = "<div class='card avatar'><div class='item item-avatar header'><img class='profile-img' src='"+picture.data.url+"'> <h2>"+name+"</h2></div></div>";
+
+            document.getElementById("profileHeader").innerHTML = htmlHeader;
+            document.getElementById("login-button").className = "hide";
+
+            document.getElementsByClassName("sign-in-text")[0].className = "sign-in-text hide";
+            document.getElementsByClassName("success")[0].className -= "hide";
+            
+        }, function(error) {
+            alert("Error: " + error);
+        });
+    }
+
+
     $scope.facebookLogin = function() {
       console.log('Logging in with Facebook');
       $cordovaOauth.facebook("1466619133643813", ["email", "public_profile"], {redirect_uri: "http://localhost/callback"}).then(function(result) {
         $localStorage.accessToken = result.access_token;
-        displayData($http, result.access_token);
+        displayData(result.access_token);
         $scope.loggedIn = true;
         $state.go("tab.my-trips");
         //$state.go($stateParams.from); <-- this crashes the app`
